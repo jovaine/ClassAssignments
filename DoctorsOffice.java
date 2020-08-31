01b6eb49a268b4afcbdc8028f3e2c18161cad26f
jovaine <70172902+jovaine@users.noreply.github.com>
2020-08-31 16:51:48 -0400

    Create DoctorsOffice.java

---
+++ DoctorsOffice.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String patientName = "Stacey Manson";
+		Boolean hasInsurance = false;
+		float temp = 102.2f;
+		Integer numVisits = 5;
+		float copay = 75f;
+		String d = "October 8, 2020";
+		Integer n = 2027867898;
+
+
+		System.out.println("Patient Details:");
+		System.out.println("Name: " + patientName);
+		System.out.println("Patient has insurance: " + hasInsurance);
+		System.out.println("Body temperature: "+temp);
+		System.out.println("Number of visits: "+numVisits);
+		System.out.println("Copay Amount is: "+copay);
+		System.out.println("Next Appointment Date is: "+d);
+		System.out.println("Contact number is: "+n);
+
+
+	}
+}
