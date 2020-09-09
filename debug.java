7aefa88201edff51908ae62356fcaeb9665c44e5
jovaine <70172902+jovaine@users.noreply.github.com>
2020-09-09 00:21:30 -0400

    Create debug.java

---
+++ debug.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Debug {
+            public static void main(String[] args) {
+
+                Scanner input=new Scanner(System.in);
+                System.out.print("Input your name: ");
+                String name=input.nextLine();
+                System.out.print("Enter your age: ");
+                String Age=input.nextLine();
+                System.out.print("Enter your DOB year: ");
+                String dob = input.nextLine();
+                System.out.println("Summary: "+"Your name is: "+name+"\n"+"Your age is: "+Age+"\n"+"Your birth year is: "+dob);
+    }
+}
